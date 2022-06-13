5e07bf5561db318d94fc909145352d295f2a0294
sohailparwany <77115237+ShanaazAhamed@users.noreply.github.com>
2022-06-14 01:57:38 +0530

    App Updated
    - Notification Fixed
    - Token Stored

--- FishTankApp2/app/src/main/java/com/example/fishtankapp/MainActivity.java
+++ FishTankApp2/app/src/main/java/com/example/fishtankapp/MainActivity.java
@@ -3,36 +3,26 @@ package com.example.fishtankapp;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView turbidityStatus;
-    TextView phValue;
-    TextView phStatus;
-    TextView availableFood;
-    TextView needFeeding;
-    DatabaseReference reference;
-
-
+    TextView turbidityStatus,phValue,phStatus, availableFood,needFeeding;
+    DatabaseReference reference_data,reference_token;
+    String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +35,25 @@ public class MainActivity extends AppCompatActivity {
         availableFood = findViewById(R.id.availableFood);
         needFeeding = findViewById(R.id.needFeeding);
 
-        reference = FirebaseDatabase.getInstance().getReference().child("DB Object name");
+        reference_data = FirebaseDatabase.getInstance().getReference().child("Sensor Data");
+        reference_token = FirebaseDatabase.getInstance().getReference().child("Tokens");
 
-        reference.addValueEventListener(new ValueEventListener() {
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("TAG", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+                        // Get new FCM registration token
+                        token = task.getResult();
+                        Log.d("TAG", token);
+                        reference_token.child(token).setValue(token);
+                    }
+                });
+
+        reference_data.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
@@ -69,53 +75,16 @@ public class MainActivity extends AppCompatActivity {
                                 turbidityStatus.setText(snapshotData.getValue().toString());
                         }
                     }
-                    sendNotification("Need Feeding", "Please fill the Feeder");
                 }
-            }
 
+            }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
     }
 
-    @Override
-    protected void onStop () {
-        super .onStop() ;
-//        startService( new Intent( this, NotificationService. class )) ;
-    }
 
-    private void sendNotification(String messageTitle,String messageBody) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-
-        String channelId = "My channel ID";
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.drawable.ic_stat_notification)
-                        .setContentTitle(messageTitle)
-                        .setContentText(messageBody)
-                        .setAutoCancel(true)
-                        .setSound(defaultSoundUri)
-                        .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // Since android Oreo notification channel is needed.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(channelId,
-                    "Channel human readable title",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            notificationManager.createNotificationChannel(channel);
-        }
-
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
-    }
 
 
 }
